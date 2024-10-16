c41356d729498ebaed9c3e1fe996a4a31ce6a09f
lilili87222 <li_li_li87222@163.com>
2024-10-16 21:49:18 +0800

    代码生成器初版本

---
+++ fastserver/generator/database.go
@@ -0,0 +1,77 @@
+package generator
+
+import (
+	"fmt"
+	"github.com/glebarez/sqlite"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+// 全局mysql数据库变量
+var DB *gorm.DB
+
+func InitDatabase() {
+	if Instance.Database.Type == "mysql" {
+		initMysql()
+	} else if Instance.Database.Type == "sqlite" {
+		initSqlLite()
+	} else {
+		panic(fmt.Errorf("mysql and sqllite support by default,不支持的数据库类型: %s", Instance.Database.Type))
+	}
+}
+
+// 初始化mysql数据库
+func initMysql() {
+	mysqlConfig := Instance.Database.MysqlConfig
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&collation=%s&%s",
+		mysqlConfig.Username,
+		mysqlConfig.Password,
+		mysqlConfig.Host,
+		mysqlConfig.Port,
+		mysqlConfig.Database,
+		mysqlConfig.Charset,
+		mysqlConfig.Collation,
+		mysqlConfig.Query,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+			//TablePrefix:   Instance.Generator.TablePrefix,
+		},
+		// 禁用外键(指定外键时不会在mysql创建真实的外键约束)
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+
+		panic(fmt.Errorf("初始化mysql数据库异常: %v", err))
+	}
+	// 开启mysql日志
+	if mysqlConfig.LogMode {
+		db.Debug()
+	}
+	DB = db
+	createTables()
+
+}
+
+// 初始化sqllite数据库
+func initSqlLite() {
+	sqlConfig := Instance.Database.SqlLiteConfig
+	db, err := gorm.Open(sqlite.Open(sqlConfig.FilePath), &gorm.Config{
+		// 禁用外键(指定外键时不会在sqlite创建真实的外键约束)
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+
+		panic(fmt.Errorf("初始化sqlite数据库异常: %v", err))
+	}
+	DB = db
+	createTables()
+
+}
+
+// 自动迁移表结构
+func createTables() {
+
+}
--- fastserver/go.mod
+++ fastserver/go.mod
@@ -25,6 +25,7 @@ require (
 	golang.org/x/crypto v0.28.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/mysql v1.5.7
+	gorm.io/gen v0.3.26
 	gorm.io/gorm v1.25.12
 )
 
@@ -89,7 +90,9 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
@@ -97,8 +100,10 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/datatypes v1.1.1-0.20230130040222-c43177d3cf8c // indirect
 	gorm.io/driver/postgres v1.5.7 // indirect
 	gorm.io/driver/sqlserver v1.5.3 // indirect
+	gorm.io/hints v1.1.0 // indirect
 	gorm.io/plugin/dbresolver v1.5.3 // indirect
 	modernc.org/libc v1.22.2 // indirect
 	modernc.org/mathutil v1.5.0 // indirect
